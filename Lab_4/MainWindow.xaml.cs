31f5bd4ff895c5c73389da7e375b74941c9fa6a0
Volodymyr <volodymyrkostiv2001@gmail.com>
2022-09-19 03:33:33 +0300

    Lab 4: Started

---
+++ Lab_4/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace Lab_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lab_4/ViewModels/HomeScreenViewModel.cs
@@ -0,0 +1,125 @@
+﻿using CommonWPF.Helpers;
+using CommonWPF.Interfaces;
+using CommonWPF.ViewModels;
+using Lab_3.Enums;
+using Lab_3.Models;
+using Microsoft.Win32;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab_4.ViewModels
+{
+    internal class HomeScreenViewModel : BaseViewModel
+    {
+        #region fields
+
+        private const KeyLength LengthOfKey = KeyLength.Bytes_16;
+        private const string Password = "Password";
+
+        private readonly RC5 _rc5;
+        private readonly RSACryptoServiceProvider _rsa;
+
+        private const int EncipherBlockSizeRSA = 64;
+        private const int DecipherBlockSizeRSA = 128;
+
+        private string _filepathToBeEnciphered;
+
+        #endregion fields
+
+        #region properties
+
+        public string InputFilePath { get; private set; }
+        public bool OperationActive { get; private set; }
+
+        #endregion properties
+
+        #region constructors
+
+        public HomeScreenViewModel(IChangeViewModel viewModelChanger) : base(viewModelChanger)
+        {
+            _rc5 = new RC5(WordType.Word_32);
+            _rsa = new RSACryptoServiceProvider();
+
+            EncryptFileCommand = new RelayCommand(EncryptFile);
+            DecryptFileCommand = new RelayCommand(DecryptFile);
+        }
+
+        #endregion constructors
+
+        #region commands
+
+        public ICommand EncryptFileCommand { get; set; }
+        private void EncryptFile()
+        {
+            if (ChooseFile())
+            {
+                try
+                {
+                    OperationActive = true;
+
+                    //var hashedKey = Encoding.UTF8.GetBytes(Password).GetMD5HashedKeyForRC5(PasswordLength);
+                    //var decodedFileContent = await Task.Run(() => rc5.Decrypt(InputFilePath, (int)NumOfRounds, hashedKey));
+
+                    MessageBox.Show("Encrypted", "RC5", MessageBoxButton.OK);
+                    OperationActive = false;
+                }
+                catch (Exception ex)
+                {
+                    OperationActive = false;
+                    MessageBox.Show(ex.Message, "Pizda Encryptiony");
+                }
+            }
+        }
+
+        public ICommand DecryptFileCommand { get; set; }
+        private void DecryptFile()
+        {
+            if (ChooseFile())
+            {
+                try
+                {
+                    OperationActive = true;
+
+                    //var hashedKey = Encoding.UTF8.GetBytes(Password).GetMD5HashedKeyForRC5(PasswordLength);
+                    //var decodedFileContent = await Task.Run(() => rc5.Decrypt(InputFilePath, (int)NumOfRounds, hashedKey));
+
+                    MessageBox.Show("Decrypted", "RC5", MessageBoxButton.OK);
+                    OperationActive = false;
+                }
+                catch (Exception ex)
+                {
+                    OperationActive = false;
+                    MessageBox.Show(ex.Message, "Pizda decryptiony");
+                }
+            }
+        }
+
+        #endregion commands
+
+        #region methods
+
+        private bool ChooseFile()
+        {
+            OperationActive = true;
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Title = "Choose input file",
+                InitialDirectory = @"D:\Program Files (x86)\Origin Games\Battlefield 1",
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                InputFilePath = openFileDialog.FileName;
+                return true;
+            }
+            OperationActive = false;
+
+            return false;
+        }
+
+        #endregion methods
+    }
+}
